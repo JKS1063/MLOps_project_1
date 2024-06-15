5a132850a7fc0863c6361c67aa69bd2d4127947a
JKS1063 <jayakrishnasekhar.reddy@outlook.com>
2024-06-15 17:03:41 +0530

    Model Training and Evaluating

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -17,5 +18,29 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))): # for loop for all models
+            model = list(models.values())[i] # storing model in model variable
+
+            model.fit(X_train, y_train) # training model
+
+            y_train_pred = model.predict(X_train) # predicting on training data
+
+            y_test_pred = model.predict(X_test) # predicting on test data
+
+            training_model_score = r2_score(y_train, y_train_pred) # r2 score on training data
+
+            test_model_score = r2_score(y_test, y_test_pred) # r2 score on test data
+
+            report[list(models.keys())[i]] = test_model_score # storing r2 score in report dictionary for each model
+
+        return report
+    
     except Exception as e:
         raise CustomException(e, sys)
